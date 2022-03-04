13d393f192c20259f264200bd139febd9036c065
Roberto Rolo <robertorolo@gmail.com>
2022-03-04 07:51:19 -0300

    message on outliers function

--- helper_functions.py
+++ helper_functions.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 def flag_outliers(array, iqr_distance = 1.5):
+    
     upper_quartile = np.percentile(array, 75)
     lower_quartile = np.percentile(array, 75)
     IQR = (upper_quartile - lower_quartile) * iqr_distance
     quartileSet = (lower_quartile - IQR, upper_quartile + IQR)
+    
     f = np.logical_and(array > quartileSet[0], array < quartileSet[1])
+
+    print('{} samples was flagged as outliers.'.format(np.sum(f)))
 
     return f
