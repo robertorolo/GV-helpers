fb8d49d39800045c394121a5ea271e38695484ce
Roberto Rolo <robertorolo@gmail.com>
2022-03-09 15:51:34 -0300

    cat1 in cat2

---
+++ cat1_in_cat2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cat1_in_cat2(cat1, cat2, outfl='cat1incat2.png'):
+    fig = plt.figure(figsize=(12,8))
+
+    x_labels = np.unique(cat1)
+    x_ticks = [i for i in range(len(x_labels))]
+
+    u_cats2 = np.unique(cat2)
+
+    heights_sum = np.zeros(len(x_labels))
+
+    for idx, i in enumerate(u_cats2):
+        cat2_filter = cat2 == i
+        
+        heights = []
+        
+        for j in x_labels:
+            cat1_filter = cat1 == j
+            both = np.logical_and(cat2_filter, cat1_filter)
+            heights.append(np.sum(both))
+
+        if idx == 0:
+            plt.bar(x_ticks, heights, label=i)
+        else:
+            plt.bar(x_ticks, heights, label=i, bottom=heights_sum)
+
+        heights_sum = heights_sum + np.array(heights)
+
+    plt.xticks(x_ticks, x_labels, rotation='vertical')
+    plt.ylabel('# samples')
+    plt.legend()
+
+    plt.savefig(outfl, bbox_inches='tight', facecolor='white')
