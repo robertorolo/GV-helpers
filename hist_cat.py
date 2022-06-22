2859ed3acbbed8c4d440be6f3d1c77887633ab68
Roberto Rolo <robertorolo@gmail.com>
2022-06-22 13:48:43 -0300

    new hist cat

--- hist_cat.py
+++ hist_cat.py
@@ -1,18 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
-def hist_cat(cat, title, outfl):
+def hist_cat(cat, title, outfl, cmap='jet'):
     fig = plt.figure(figsize=(12,8))
 
-    defined_filter = [isinstance(x, str) for x in cat]
-    cat = cat[defined_filter]
+    #defined_filter = [isinstance(x, str) for x in cat]
+    #cat = cat[defined_filter]
 
     x_labels = np.unique(cat)
     x_ticks = [i for i in range(len(x_labels))]
+    cmap = matplotlib.cm.get_cmap(cmap, len(x_labels))
+    colors = [matplotlib.colors.rgb2hex(cmap(i)[:3]) for i in range(cmap.N)]
 
     heights = [np.sum(cat==i) for i in x_labels]
 
-    plt.bar(x_ticks, heights)
+    plt.bar(x_ticks, heights, color=colors)
 
     for x, y in zip(x_ticks, heights):
         plt.annotate(str(y), (x, y), ha='center')
